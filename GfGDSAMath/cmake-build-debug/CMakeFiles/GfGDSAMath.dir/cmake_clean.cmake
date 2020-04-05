4c700eafdc55082064838f9e89cf7a0629b9897e
jimboweb <jimboweb@gmail.com>
2020-04-05 13:20:20 -0400

    first commit

---
+++ GfGDSAMath/cmake-build-debug/CMakeFiles/GfGDSAMath.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GfGDSAMath.dir/main.cpp.o"
+  "GfGDSAMath.pdb"
+  "GfGDSAMath"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GfGDSAMath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ GfGDSAMath/cmake-build-debug/CMakeFiles/GfGDSAMath.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GfGDSAMath.
+# This may be replaced when dependencies are built.
