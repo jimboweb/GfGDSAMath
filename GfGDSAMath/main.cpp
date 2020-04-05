8594763c1342163ac93d9648ff4c1e2d6752c16e
jimboweb <jimboweb@gmail.com>
2020-04-05 13:26:09 -0400

    numDigitsNaive
    added naive function for number of digits

--- GfGDSAMath/main.cpp
+++ GfGDSAMath/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+int numDigitsNaive(int n){
+    int rtrn = 0;
+    while(n>=1) {
+        n /= 10;
+        rtrn++;
+    }
+    return rtrn;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    printf("%d has %d digits\n",17,(numDigitsNaive(17)));
+    printf("%d has %d digits\n",5,(numDigitsNaive(5)));
+    printf("%d has %d digits\n",1265,(numDigitsNaive(1265)));
+    printf("%d has %d digits\n",100,(numDigitsNaive(100)));
+
     return 0;
 }
